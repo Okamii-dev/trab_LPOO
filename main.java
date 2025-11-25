27ec592e8ffab33fd79b042f24531d4061a98399
Alexsander <namelesslifeuser@gmail.com>
2025-11-25 00:15:28 -0300

    Delete main.java

--- main.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.SwingUtilities;
-import view.TelaPrincipal;
-
-public class main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new TelaPrincipal().setVisible(true));
-    }
-}
